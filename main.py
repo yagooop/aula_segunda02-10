106616853988ab48b6d2169cec30fcf03d607284
yagooop <146749558+yagooop@users.noreply.github.com>
2023-10-02 12:04:39 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+num1=int(input("digite o primeiro numero: "))
+num2=int(input("digite o segundo numero: "))
+
+soma = num1 + num2 
+
+sub = num1 - num2
+
+divisao = num1 / num2
+
+multiplicacao = num1 * num2 
+
+print(f"soma: {soma}")
+
+
+print(f"subtracao: {sub}")
+
+
+print(f"divisao: {divisao}")
+                  
+                  
+print(f"multiplicacao: {multiplicacao}")
+
